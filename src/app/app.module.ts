49e520a1613c35a4e5ec44251406f282712333bf
lancelynyrd <pinedaclp@gmail.com>
2016-10-20 20:20:45 +0800

    updating required changes base on xmodule installation guide

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,43 @@
 import { NgModule } from '@angular/core';
 import { IonicApp, IonicModule } from 'ionic-angular';
 import { MyApp } from './app.component';
-import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
-import { TabsPage } from '../pages/tabs/tabs';
-
+/*import { LoginPage } from '../pages/login/login';
+import { RegisterPage } from '../pages/register/register';
+import { PasswordPage } from '../pages/password/password';
+import { ResignPage } from '../pages/resign/resign';
+import { ForumCategoryPage } from '../pages/forum-category/forum-category';
+import { PostListPage } from '../pages/post-list/post-list';
+import { PostEditPage } from '../pages/post-edit/post-edit';*/
+import { XModule } from '../xmodule/modules/core';
 @NgModule({
   declarations: [
     MyApp,
-    AboutPage,
-    ContactPage,
     HomePage,
-    TabsPage
+    /*LoginPage,
+    RegisterPage,
+    PasswordPage,
+    ResignPage,
+    ForumCategoryPage,
+    PostListPage,
+    PostEditPage*/
   ],
   imports: [
+    XModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    AboutPage,
-    ContactPage,
     HomePage,
-    TabsPage
+    /*LoginPage,
+    RegisterPage,
+    PasswordPage,
+    ResignPage,
+    ForumCategoryPage,
+    PostListPage,
+    PostEditPage*/
   ],
-  providers: []
+  providers: [ ]
 })
 export class AppModule {}
